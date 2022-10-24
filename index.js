fb46d628946860373847a31c1a705142a8e84aa1
dankturtle <abrowncownow@gmail.com>
2022-10-24 15:31:35 -0700

    index.js and Manager class added

--- index.js
+++ index.js
@@ -1,2 +1,60 @@
+//import and declare constants-----
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager.js');
+const chalk = require('chalk');
+var team = [];
+var addMember;
+//import and declare constants------
 
+
+//declare functions-----
+function getManager() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: `What is the ${chalk.bold("Team Manager's name?")}`,
+                name: "name",
+            },
+            {
+                type: "input",
+                message: `Enter the ${chalk.bold("Team Manager's employee id:")}`,
+                name: "id",
+            },
+            {
+                type: "input",
+                message: `Enter the ${chalk.bold("Team Manager's email:")}`,
+                name: "email",
+            },
+            {
+                type: "input",
+                message: `Enter the ${chalk.bold("Team Manager's office number:")}`,
+                name: "number",
+            },
+        ])
+        .then((answers) => {
+            team.push(new Manager(answers));
+            console.log(team);
+            menu();
+        });
+}
+
+function menu() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: `Select the type of team member to add or select "finished" to finish adding.`,
+                name: "menu",
+                choices: ["Engineer", "Intern", "finished"]
+            },
+        ])
+        .then((answers) => {
+            
+        });
+}
+//declare functions------
+
+//main-----
+getManager();
+//main-----
